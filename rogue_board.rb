7923dd970880e1c37df4dd608b92bf8a63431b61
Jean Lange <langeje@us.ibm.com>
2018-03-09 10:12:57 -0500

    use cells and place a character

--- rogue_board.rb
+++ rogue_board.rb
@@ -1,7 +1,29 @@
+class Cell
+  # in ruby, def initialize is the constructor
+  # a constructor is the method that's called when you create a new instance of that class
+  # constructors are for setting up class data to start
+  def initialize
+    @has_character = false
+  end
+
+  def place_character
+    @has_character = true
+  end
+
+  def to_s
+    if @has_character
+      "@"
+    else
+      "-"
+    end
+  end
+end
+
 class RogueBoard
   def initialize
-    size = 2
-    @grid = Array.new(size) { Array.new(size) {"_"}} 
+    size = 5
+    @grid = Array.new(size) { Array.new(size) {Cell.new}}
+    @grid[0][1].place_character
   end
 
   def rows
